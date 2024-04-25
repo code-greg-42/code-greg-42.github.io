f4bc0303168cedd6a3fd0e437d50c2bd78aeafa1
greg <grandersson1991@gmail.com>
2024-04-25 13:31:29 -0400

    testing updates to github pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
       <header className="App-header">
         <h1>Hello, world!</h1>
         <button className="btn btn-primary">Click me!</button>
+        <p>This is the beginning of the portfolio</p>
       </header>
     </div>
   );
