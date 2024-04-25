568fac131ff42366154dbd02becee04296c20eba
greg <grandersson1991@gmail.com>
2024-04-25 13:03:22 -0400

    updated to use react and boostrap

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, world!</h1>
+        <button className="btn btn-primary">Click me!</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
