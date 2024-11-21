3355f29b6a01e2c338422617874440e8bb4e8b57
greg <grandersson1991@gmail.com>
2024-11-21 16:02:17 -0500

    updated portfolio site

--- src/App.js
+++ src/App.js
@@ -9,11 +9,6 @@ function App() {
       <Portfolio />
       <div id="about" className="container mt-5">
       <h2>About Me</h2>
-        <p>I am Gregory Andersson, a dedicated and passionate game developer with a rich background in computer science and digital art. I specialize in creating immersive gaming experiences that engage and excite players.</p>
-  
-        <p>After graduating with a degree in Computer Science from the University of Game Design, I have worked with several indie game studios, contributing to both major releases and experimental projects. My technical expertise lies in gameplay programming and artificial intelligence, but I am also skilled in 3D modeling and animation.</p>
-  
-        <p>Outside of work, I enjoy participating in game jams, exploring the latest in VR technology, and sharing my knowledge through workshops and community events.</p>
       </div>
     </div>
   );
--- src/MyNavbar.js
+++ src/MyNavbar.js
@@ -4,7 +4,7 @@ function MyNavbar() {
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Gregory Andersson</a>
+                <a className="navbar-brand" href="https://github.com/code-greg-42">Greg</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
